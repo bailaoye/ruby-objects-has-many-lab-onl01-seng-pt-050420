79cb5b9f6bf901991ca145b30e4ce3b58fb186b0
Jeff Bai <j.bai@wustl.edu>
2020-05-29 23:57:00 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -16,7 +16,7 @@ class Post
     author.songs << self
   end
 
-  def author_title
+  def author_name
     #logic here : condition? if true : if false
     self.author ? self.author.name : nil
   end
