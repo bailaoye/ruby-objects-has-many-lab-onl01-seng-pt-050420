2df45599a5421296e85b67d24e7919d0b9f04be9
Jeff Bai <j.bai@wustl.edu>
2020-05-29 23:20:09 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,10 +5,13 @@ class Artist
     @songs = []
   end
 
-  def add_song_by_name(song)
-    @songs << song
+  def add_song(song)
     song.artist = self
   end
+
+  def add_song_by_name(song)
+    @songs << song
+
 
   def songs
     @songs
